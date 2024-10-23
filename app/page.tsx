f73633aad305e68f4eaf3853c89222052aec36ae
Lucas Lobell <lucaslobell2@gmail.com>
2024-10-23 11:44:45 -0300

    floating nav

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@ import Contact from "@/components/Contact";
 import Projects from "@/components/Projects";
 import Resume from "@/components/Resume";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main>
       <div className="bg-black">
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Projects />
         <Resume />
---
+++ data/index.ts
@@ -0,0 +1,5 @@
+export const navItems = [
+    { name: "Projects", link: "#Projects" },
+    { name: "Resume", link: "#Resume" },
+    { name: "Contact", link: "#Contact" },
+  ]
