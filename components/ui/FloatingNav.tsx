6c02ffbdc0c623e8d546d2d862beb5d47cb2bdd1
Lucas Lobell <lucaslobell2@gmail.com>
2024-10-25 11:44:26 -0300

    i guess this is something but not great, mabraing is cooked

--- components/ui/FloatingNav.tsx
+++ components/ui/FloatingNav.tsx
@@ -21,58 +21,43 @@ export const FloatingNav = ({
   className?: string;
 }) => {
   const { scrollYProgress } = useScroll();
-
   const [visible, setVisible] = useState(false);
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
-    // Check if current is not undefined and is a number
     if (typeof current === "number") {
-      let direction = current! - scrollYProgress.getPrevious()!;
-
-      if (scrollYProgress.get() < 0.) {
-        setVisible(false);
-      } else {
-        if (direction < 0) {
-          setVisible(true);
-        } else {
-          setVisible(false);
-        }
-      }
+      let direction = current - scrollYProgress.getPrevious()!;
+      setVisible(scrollYProgress.get() >= 0.075 && direction < 0);
     }
   });
 
   return (
     <AnimatePresence mode="wait">
-      <motion.div
-        initial={{
-          opacity: 1,
-          y: -100,
-        }}
-        animate={{
-          y: visible ? 0 : -100,
-          opacity: visible ? 1 : 0,
-        }}
-        transition={{
-          duration: 0.25,
-        }}
-        className={cn(
-          "flex max-w-fit fixed top-5 inset-x-0 mx-auto border rounded-[12px] shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.25),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-8 py-4 items-center justify-center space-x-4 border-white/[0.2] bg-black/[0.9]",
-          className
-        )}
-      >
-        {navItems.map((navItem: any, idx: number) => (
-          <Link
-            key={`link=${idx}`}
-            href={navItem.link}
-            className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
-            )}
-          >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="text-sm !cursor-pointer">{navItem.name}</span>
-          </Link>
-        ))}
-      </motion.div>
+      {visible && (
+        <motion.div
+          initial={{ opacity: 1, y: -100 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -100, opacity: 0 }}
+          transition={{ duration: 0.25 }}
+          className={cn(
+            "flex max-w-fit fixed top-5 inset-x-0 mx-auto z-[5000] px-8 py-4 items-center justify-center space-x-4 bg-black",
+            "floatingNav", // Apply the animated border class
+            className
+          )}
+        >
+          {navItems.map((navItem, idx) => (
+            <Link
+              key={`link=${idx}`}
+              href={navItem.link}
+              className={cn(
+                "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+              )}
+            >
+              <span className="block sm:hidden">{navItem.icon}</span>
+              <span className="text-sm cursor-pointer">{navItem.name}</span>
+            </Link>
+          ))}
+        </motion.div>
+      )}
     </AnimatePresence>
   );
 };
